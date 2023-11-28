5e1efd7274a1cf8a5ee9909b16d561a297f9305a
semanticdata <contact@miguelpimentel.do>
2023-11-28 12:08:52 -0600

    formatting

--- background.js
+++ background.js
@@ -8,7 +8,7 @@ function onError(e) {
 
 function setButtonIcon(imageURL) {
   try {
-    browser.browserAction.setIcon({path: imageURL});
+    browser.browserAction.setIcon({ path: imageURL });
   } catch (e) {
     onError(e);
   }
@@ -26,16 +26,16 @@ function createPinnedTab() {
 
 function handleSearch(whatsappTabs) {
   //console.log("currentTabId: " + currentTabId);
-  if(whatsappTabs.length > 0) {
+  if (whatsappTabs.length > 0) {
     //console.log("there is a calendar tab");
     whatsappTabId = whatsappTabs[0].id;
-    if(whatsappTabId === currentTabId) {
+    if (whatsappTabId === currentTabId) {
       //console.log("I'm in the drive tab");
-      browser.tabs.update(previousTab, {active: true,});
+      browser.tabs.update(previousTab, { active: true, });
     } else {
       //console.log("I'm NOT in the drive tab");
       previousTab = currentTabId;
-      browser.tabs.update(whatsappTabId, {active: true,});
+      browser.tabs.update(whatsappTabId, { active: true, });
     }
     setButtonIcon(whatsappTabs[0].favIconUrl);
   } else {
@@ -48,7 +48,7 @@ function handleSearch(whatsappTabs) {
 function handleClick(tab) {
   //console.log("*********Button clicked*********");
   currentTabId = tab.id;
-  var querying = browser.tabs.query({url: "*://web.whatsapp.com/*"});
+  var querying = browser.tabs.query({ url: "*://web.whatsapp.com/*" });
   querying.then(handleSearch, onError);
 };
 
